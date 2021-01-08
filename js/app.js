a6e21ef223abbf37811803fbddeb03b2ec487c19
Djessa <s.djessa.2000@gmail.com>
2021-01-08 07:48:39 +0100

    Mise en form css, add onclick attribute in button, add keypress action

--- js/app.js
+++ js/app.js
@@ -1,19 +1,23 @@
-let content = document.querySelector('.content');
-let i = 0;
-document.querySelector('#add').addEventListener('click', () => {
+
+document.addEventListener("keypress", (event) => {
+    if (event.keyCode === 13) {
+        add();
+    }
+});
+add = () => {
     const todo = document.querySelector('input').value;
     if (todo.length > 0) {
-        content.insertAdjacentHTML('afterbegin', `<div class="item">${todo}<span><i  onclick=check(this) class="fa fa-check"></i><i  onclick=trash(this) class="fa fa-trash"></i></span></div>`);
+        if (document.querySelector('.content').childNodes.length < 7)
+            document.querySelector('.content').insertAdjacentHTML('afterbegin', `<div class="item">${todo}<span><i  onclick=check(this) class="fa fa-check"></i><i  onclick=trash(this) class="fa fa-trash"></i></span></div>`);
     }
     document.querySelector('input').value = "";
-});
+}
+
 check = (action) => {
     const event = action.parentElement.parentElement;
     event.querySelector('.fa-check').remove();
     event.classList.remove('item');
     event.classList.add('lu');
 }
-trash = (action) => {
-    const event = action.parentElement.parentElement;
-    event.remove();
-}
+
+trash = (action) => action.parentElement.parentElement.remove();
